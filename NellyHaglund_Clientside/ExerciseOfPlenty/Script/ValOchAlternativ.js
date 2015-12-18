037bf09679372f18ebed5a53cf73646f67aa9105
Nelly Haglund <haglundnelly@gmail.com>
2015-12-18 19:08:23 +0100

    Did part 1 in exercise 2

---
+++ NellyHaglund_Clientside/ExerciseOfPlenty/Script/ValOchAlternativ.js
@@ -0,0 +1,10 @@
+﻿document.getElementById("button_IfFirstNumberGreatest_ThenShow").addEventListener('click', checkIfFirstNumberIsTheGreatest, true);
+
+function checkIfFirstNumberIsTheGreatest() {
+    var numbers = document.getElementById("input_numbers").value.split(',');
+    if (numbers[0] > numbers[1]) {
+        document.getElementById("IfFirstNumberGreatest_ThenShow").innerHTML =  "The first number is the greatest:" + numbers[0];
+    } else {
+        document.getElementById("IfFirstNumberGreatest_ThenShow").innerHTML = "The last number is the greatest...";
+    }
+}
