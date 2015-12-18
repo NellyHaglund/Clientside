54d56b10641ec5b8175b04b545dd4792b4b29e2b
Nelly Haglund <haglundnelly@gmail.com>
2015-12-18 16:07:37 +0100

    Did first part in exercise 1

--- NellyHaglund_Clientside/ExerciseOfPlenty/Script/Sekvensboxar.js
+++ NellyHaglund_Clientside/ExerciseOfPlenty/Script/Sekvensboxar.js
@@ -1,1 +1,7 @@
-﻿
+﻿document.getElementById("CalculateSquareOfTwo").addEventListener('click', calculateSqareOfTwo, true);
+
+function calculateSqareOfTwo() {
+    var numberToCalc = Number(document.getElementById("squareOfTwo").value);
+    var result = numberToCalc * numberToCalc;
+    document.getElementById("squareOfTwoResult").innerHTML = result;
+}
