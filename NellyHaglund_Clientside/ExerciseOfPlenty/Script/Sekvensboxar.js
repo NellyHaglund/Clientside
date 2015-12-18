28d774fc2542215a2fb669f3ff90d760cc0b4830
Nelly Haglund <haglundnelly@gmail.com>
2015-12-18 16:27:57 +0100

    Part 2 in exercise 1

--- NellyHaglund_Clientside/ExerciseOfPlenty/Script/Sekvensboxar.js
+++ NellyHaglund_Clientside/ExerciseOfPlenty/Script/Sekvensboxar.js
@@ -1,7 +1,22 @@
 ﻿document.getElementById("CalculateSquareOfTwo").addEventListener('click', calculateSqareOfTwo, true);
+document.getElementById("calculateSum").addEventListener('click', calculateSum, true);
+
+
 
 function calculateSqareOfTwo() {
     var numberToCalc = Number(document.getElementById("squareOfTwo").value);
     var result = numberToCalc * numberToCalc;
     document.getElementById("squareOfTwoResult").innerHTML = result;
+}
+
+function calculateSum() {
+    var baseSalary = 15000;
+    var salesSum = document.getElementById("salesSum").value;
+    var salesPercent = 0.09;
+    var extraToSalary = salesSum * salesPercent;
+    var salary = baseSalary + extraToSalary;
+    document.getElementById("sales_sum").innerHTML = salary;
+
+    
+
 }
