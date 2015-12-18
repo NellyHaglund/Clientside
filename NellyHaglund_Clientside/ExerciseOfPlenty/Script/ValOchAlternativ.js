dd37912d64dd8aa214cb2a0aa527fcca9400a8db
Nelly Haglund <haglundnelly@gmail.com>
2015-12-18 19:35:16 +0100

    Part 3 in exercise 2

--- NellyHaglund_Clientside/ExerciseOfPlenty/Script/ValOchAlternativ.js
+++ NellyHaglund_Clientside/ExerciseOfPlenty/Script/ValOchAlternativ.js
@@ -1,5 +1,6 @@
 ﻿document.getElementById("button_IfFirstNumberGreatest_ThenShow").addEventListener('click', checkIfFirstNumberIsTheGreatest, true);
 document.getElementById("button_IfFirstNumberIsTwiceAsSecond_ThenToBigNumber").addEventListener('click', checkIfFirstNumberIsTwiceTheSecond, true);
+document.getElementById("button_CheckIfNumberIsDivisibleBy3").addEventListener('click',checkIfNumberDivisibleBy3 , true);
 
 function checkIfFirstNumberIsTheGreatest() {
     var numbers = document.getElementById("input_numbers_Greatest").value.split(',');
@@ -15,6 +16,15 @@ function checkIfFirstNumberIsTwiceTheSecond() {
         document.getElementById("IfFirstNumberIsTwiceAsSecond_ThenToBigNumber").innerHTML = "TO BIG NUMBER!";
     } else {
         document.getElementById("IfFirstNumberIsTwiceAsSecond_ThenToBigNumber").innerHTML = "";
+
+    }
+}
+function checkIfNumberDivisibleBy3() {
+    var number = document.getElementById("input_number_Modulo").value;
+    if (number%3 === 0) {
+        document.getElementById("showNumberIfDivisibleBy3WithNoRest").innerHTML = "Number can be divided by three (number%3): " + number;
+    } else {
+        document.getElementById("showNumberIfDivisibleBy3WithNoRest").innerHTML = "Number cannot be divided by three (number%3): ";
 
     }
 }
