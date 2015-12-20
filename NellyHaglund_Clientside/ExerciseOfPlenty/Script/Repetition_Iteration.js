b48b9007a133cdb897a893f7e00bfc5d97584ea3
Nelly Haglund <haglundnelly@gmail.com>
2015-12-20 17:52:06 +0100

    Part 2, exercise 3

--- NellyHaglund_Clientside/ExerciseOfPlenty/Script/Repetition_Iteration.js
+++ NellyHaglund_Clientside/ExerciseOfPlenty/Script/Repetition_Iteration.js
@@ -1,4 +1,5 @@
 ﻿document.getElementById("readSerieOfNumber").addEventListener('click', returnSerieOfNumberUntilZero, true);
+document.getElementById("button_EvenNumbersBetween0And100").addEventListener('click', returnEvenNumbersBetween0And100, true);
 
 function returnSerieOfNumberUntilZero() {
     var numbers = document.getElementById("serieOfNumbers").value;
@@ -10,6 +11,14 @@ function returnSerieOfNumberUntilZero() {
                 document.getElementById("resultUntilZero").innerHTML += "Ohh, zero...";
                 break;
             }
+        }
+    }
+}
+function returnEvenNumbersBetween0And100() {
+    for (var i = 2; i <= 100 ; i++) {
+        if (i % 2 == 0) {
+            document.getElementById("EvenNumbersBetween0And100").innerHTML += i + " ";
+
         }
     }
 }
