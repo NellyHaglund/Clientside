78e4fe1a1caeab7b3386849aacf6990270738513
Nelly Haglund <haglundnelly@gmail.com>
2015-12-22 17:24:31 +0100

    Created a constructor function named Product

--- WebApplication33/Exercise2/MenuPage2.js
+++ WebApplication33/Exercise2/MenuPage2.js
@@ -4,6 +4,7 @@
    Please use the same html syntax of an article as in exercise1.
 2. create Product objects with constructor notation, 
    use the parameters name, size, price, dayOfTheWeek  (page. 108 in book)
+ 
 3. add 7 drinks to the page using Product.
 4. add 7 pizzas to the page using Product.
 5. add 7 subs to the page using Product.
@@ -11,3 +12,10 @@
 */
 //DRINKS
 //Subs
+function Product(dayOfTheWeek, name, price, size, img) {
+    this.dayOfTheWeek = dayOfTheWeek;
+    this.name = name;
+    this.price = price;
+    this.size = size;
+    this.img = img;
+}
