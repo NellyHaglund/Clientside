0252fb075b71595696a9e37ac255c9762f635f43
Nelly Haglund <haglundnelly@gmail.com>
2015-12-23 13:35:12 +0100

    Added function handleRequest and addProductFromJson

--- WebApplication33/Exercise2/MenuPage2.js
+++ WebApplication33/Exercise2/MenuPage2.js
@@ -13,6 +13,9 @@
 //DRINKS
 //Subs
 
+handleRequest();
+
+
 //Constructor function
 function Product(dayOfTheWeek, name, price, size, img) {
     this.dayOfTheWeek = dayOfTheWeek;
@@ -22,53 +25,56 @@ function Product(dayOfTheWeek, name, price, size, img) {
     this.img = img;
 }
 
+var listOfProducts = new Array;
 //Read json file - hamburgers
-var request;
-if (window.XMLHttpRequest) {
-    request = new XMLHttpRequest();
-} else {
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-request.open("GET", "JsonFiles/hamburger.json");
 
-request.onreadystatechange = function() {
-    var listOfProducts = new Array;
-    if (request.readyState === 4 && request.status === 200) {
-        var jsonHamburgers = JSON.parse(request.responseText);
-        for (var i = 0; i < jsonHamburgers.hamburgers.length; i++) {
-            listOfProducts.push(new Product(jsonHamburgers.hamburgers[i].dayOfTheWeek, jsonHamburgers.hamburgers[i].name, jsonHamburgers.hamburgers[i].price, jsonHamburgers.hamburgers[i].size, jsonHamburgers.hamburgers[i].img));
-        }
-        alert(listOfProducts[2].price);
+function handleRequest() {
+    var request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
     }
-};
-request.send();
+    request.open("GET", "JsonFiles/hamburger.json");
+
+    request.onreadystatechange = function () {
+
+        if (request.readyState === 4 && request.status === 200) {
+            var jsonHamburgers = JSON.parse(request.responseText);
+
+            addProductFromJson(jsonHamburgers);
+            alert(listOfProducts[2].price);
+        }
+
+    };
+    request.send();
+}
+
+function addProductFromJson(jsonHamburgers) {
+    for (var i = 0; i < jsonHamburgers.hamburgers.length; i++) {
+        listOfProducts.push(new Product(jsonHamburgers.hamburgers[i].dayOfTheWeek,
+            jsonHamburgers.hamburgers[i].name,
+            jsonHamburgers.hamburgers[i].price,
+            jsonHamburgers.hamburgers[i].size,
+            jsonHamburgers.hamburgers[i].img));
+    }
+}
 
 function calculatePrice(price, dayOfTheWeek) {
     var newPrice = price;
-
     //Reduce 30% while happy hours
     var hour = new Date().getHours();
     if (hour >= 17 && hour <= 19) {
         newPrice = (price * 0.7);
     }
 
-    var daysOfTheWeek = [
-        "sunday",
-        "monday",
-        "tuseday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday"
-    ];
-
     //Reduce exta 20% if burger is burger of the day
     var currentDay = new Date().getDay();
 
-    for (var i = 0; i < daysOfTheWeek.length; i++) {
-        if (i === dayOfTheWeek && daysOfTheWeek[i] === dayOfTheWeek) {
-            newPrice *= 0.8;
-        }
+    //for (var i = 0; i < daysOfTheWeek.length; i++) {
+    if (currentDay == dayOfTheWeek) {
+        newPrice *= 0.8;
     }
+    //}
     return newPrice;
 }
