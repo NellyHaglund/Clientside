227fbfe13f544da3bc7c769bde3d2496dcccbeeb
Nelly Haglund <haglundnelly@gmail.com>
2016-01-05 21:59:59 +0100

    Fixed bug in search function

--- SearchIntegration/SearchIntegration/Scripts/Index.js
+++ SearchIntegration/SearchIntegration/Scripts/Index.js
@@ -2,14 +2,19 @@
 
     $(".checkboxes").change(function () {
         var output = "<ul class='resultList'>";
+        var searchInput = $("#search_input").val();
+        console.log(searchInput);
+        var regex = new RegExp(searchInput, "i");
         $.getJSON("Json/products.json", function (data) {
             $.each(data, function (key, value) {
-                if ($("#" + value.category).is(":checked")) {
-                    output += "<li>";
-                    output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
-                    output += "<h2>" + value.name + "</h2>" + "</br>";
-                    output += value.description;
-                    output += "</li>";
+                if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
+                    if ($("#" + value.category).is(":checked")) {
+                        output += "<li>";
+                        output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
+                        output += "<h2>" + value.name + "</h2>" + "</br>";
+                        output += value.description;
+                        output += "</li>";
+                    }
                 }
             });
             output += "</ul>";
@@ -33,8 +38,8 @@
     });
 
     $("#search_input").keyup(function () {
-        var searchInput = $(this).val();
         var output = "<ul class='resultList'>";
+        var searchInput = $(this).val();     
         var regex = new RegExp(searchInput, "i");
         $.getJSON("Json/products.json", function (data) {
             $.each(data, function (key, value) {
