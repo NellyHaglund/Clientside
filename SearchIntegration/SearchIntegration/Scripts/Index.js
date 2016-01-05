1e550e3ec7a5661255fe60305404b47b79c0b9b8
Nelly Haglund <haglundnelly@gmail.com>
2016-01-05 20:27:56 +0100

    Fixed css, page looks nice with rainbow colors as inspiration

--- SearchIntegration/SearchIntegration/Scripts/Index.js
+++ SearchIntegration/SearchIntegration/Scripts/Index.js
@@ -1,22 +1,20 @@
 ﻿$(function () {
     $("#search_input").keyup(function () {
         var searchInput = $(this).val();
-        console.log($(this).val());
-        var output = "<ul class='resultList'";
+        var output = "<ul class='resultList'>";
         var regex = new RegExp(searchInput, "i");
         $.getJSON("Json/products.json", function (data) {
             $.each(data, function (key, value) {
                 if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
-                    console.log("Found");
                     output += "<li>";
                     output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
                     output += value.name + "</br>";
                     output += value.description;
-                    output += "</li>";               
+                    output += "</li>";
                 }
-                output += "</ul>";
-                $("#searchResults").html(output);
             });
+            output += "</ul>";
+            $("#searchResults").html(output);
         });
     });
 });
