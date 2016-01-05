cf7235f9abdad113020887e219f02a54ab30bdb0
Nelly Haglund <haglundnelly@gmail.com>
2016-01-05 14:38:29 +0100

    Get data from json file into html page when searching - drinks

--- SearchIntegration/SearchIntegration/Scripts/Index.js
+++ SearchIntegration/SearchIntegration/Scripts/Index.js
@@ -1,5 +1,21 @@
-﻿$(function() {
-    $("#search_input").keyup(function() {
+﻿$(function () {
+    $("#search_input").keyup(function () {
         var searchInput = $(this).val();
+        var regex = new RegExp(searchInput, "i");
+
+        $.getJSON("Json/drinks.json", function (data) {
+            var output = "<ul>";
+            $.each(data.drinks, function (key, value) {
+                if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
+                    output += "<li>";
+                    output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
+                    output += value.name + "</br>";
+                    output += value.description;
+                    output += "</li>";
+                }
+            });
+            output += "</ul>";
+            $("#searchResults").html(output);
+        });
     });
 });
