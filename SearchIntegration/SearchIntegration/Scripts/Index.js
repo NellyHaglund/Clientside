eb5e5dcae7c50bdfca00535f223a0fed892d96b4
Nelly Haglund <haglundnelly@gmail.com>
2016-01-05 20:52:38 +0100

    Added checkboxes for each category

--- SearchIntegration/SearchIntegration/Scripts/Index.js
+++ SearchIntegration/SearchIntegration/Scripts/Index.js
@@ -1,4 +1,17 @@
 ﻿$(function () {
+    var output = "<ul class='resultList'>";
+    $.getJSON("Json/products.json", function (data) {
+        $.each(data, function (key, value) {
+                output += "<li>";
+                output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
+                output += "<h2>" + value.name + "</h2>" + "</br>";
+                output += value.description;
+                output += "</li>";
+        });
+        output += "</ul>";
+        $("#searchResults").html(output);
+    });
+
     $("#search_input").keyup(function () {
         var searchInput = $(this).val();
         var output = "<ul class='resultList'>";
@@ -8,7 +21,7 @@
                 if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
                     output += "<li>";
                     output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
-                    output += value.name + "</br>";
+                    output += "<h2>" + value.name + "</h2>" + "</br>";
                     output += value.description;
                     output += "</li>";
                 }
