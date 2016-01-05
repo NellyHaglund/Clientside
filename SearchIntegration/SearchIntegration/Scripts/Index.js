c22f0ff2d01670d3c2679eff067911ddfefd94a9
Nelly Haglund <haglundnelly@gmail.com>
2016-01-05 15:09:32 +0100

    Filter function works

--- SearchIntegration/SearchIntegration/Scripts/Index.js
+++ SearchIntegration/SearchIntegration/Scripts/Index.js
@@ -1,21 +1,22 @@
 ﻿$(function () {
     $("#search_input").keyup(function () {
         var searchInput = $(this).val();
+        console.log($(this).val());
+        var output = "<ul>";
         var regex = new RegExp(searchInput, "i");
-
-        $.getJSON("Json/drinks.json", function (data) {
-            var output = "<ul>";
-            $.each(data.drinks, function (key, value) {
+        $.getJSON("Json/products.json", function (data) {
+            $.each(data, function (key, value) {
                 if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
+                    console.log("Found");
                     output += "<li>";
                     output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
                     output += value.name + "</br>";
                     output += value.description;
-                    output += "</li>";
+                    output += "</li>";               
                 }
+                output += "</ul>";
+                $("#searchResults").html(output);
             });
-            output += "</ul>";
-            $("#searchResults").html(output);
         });
     });
 });
