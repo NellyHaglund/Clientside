21a70c2e692b0250ec2ab750664f1819ef05380c
Nelly Haglund <haglundnelly@gmail.com>
2016-01-05 21:08:21 +0100

    Fixed logic for only show products whose checkbox is checked

--- SearchIntegration/SearchIntegration/Scripts/Index.js
+++ SearchIntegration/SearchIntegration/Scripts/Index.js
@@ -1,12 +1,32 @@
 ﻿$(function () {
+
+    $(".checkboxes").change(function() {
+        var output = "<ul class='resultList'>";
+        $.getJSON("Json/products.json", function (data) {
+            $.each(data, function (key, value) {
+                if ($("#" + value.category).is(":checked")) {
+                    output += "<li>";
+                    output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
+                    output += "<h2>" + value.name + "</h2>" + "</br>";
+                    output += value.description;
+                    output += "</li>";
+                }
+            });
+            output += "</ul>";
+            $("#searchResults").html(output);
+        });
+    });
+
     var output = "<ul class='resultList'>";
     $.getJSON("Json/products.json", function (data) {
         $.each(data, function (key, value) {
+            if ($("#" + value.category).is(":checked")) {
                 output += "<li>";
                 output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
                 output += "<h2>" + value.name + "</h2>" + "</br>";
                 output += value.description;
                 output += "</li>";
+            }
         });
         output += "</ul>";
         $("#searchResults").html(output);
@@ -19,11 +39,13 @@
         $.getJSON("Json/products.json", function (data) {
             $.each(data, function (key, value) {
                 if ((value.name.search(regex) !== -1) || (value.description.search(regex) !== -1)) {
-                    output += "<li>";
-                    output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
-                    output += "<h2>" + value.name + "</h2>" + "</br>";
-                    output += value.description;
-                    output += "</li>";
+                    if ($("#" + value.category).is(":checked")) {
+                        output += "<li>";
+                        output += "<img src='" + value.img + "' alt='" + value.name + "'/>";
+                        output += "<h2>" + value.name + "</h2>" + "</br>";
+                        output += value.description;
+                        output += "</li>";
+                    }
                 }
             });
             output += "</ul>";
